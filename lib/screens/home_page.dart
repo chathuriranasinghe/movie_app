176d20440712fd20004e6fe4fd878c5f2da1785e
Chathuri Ranasinghe <chathusudarshiranasinghe@gmail.com>
2025-01-11 20:13:57 +0530

    Modify App

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/services/api_service.dart';
 
@@ -13,26 +14,53 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(
-                  onPressed: () {},
-                  icon: Icon(
-                    Icons.menu,
-                    color: Colors.black,
-                  )),
-              Text(
-                'Movie Hub',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 40.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade500,
+                  borderRadius: BorderRadius.circular(15)),
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.menu,
+                        color: Colors.black,
+                      )),
+                  Spacer(),
+                  Text(
+                    'Movie Hub',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
+            CarouselSlider(
+              options: CarouselOptions(height: 400.0, autoPlay: true),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.symmetric(horizontal: 5.0),
+                        decoration: BoxDecoration(color: Colors.amber),
+                        child: Text(
+                          'text $i',
+                          style: TextStyle(fontSize: 16.0),
+                        ));
+                  },
+                );
+              }).toList(),
+            )
+          ],
+        ),
       ),
     );
   }
