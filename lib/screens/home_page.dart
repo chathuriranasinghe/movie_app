9a657b22392e963d7535b98d29a08fcddecfe8fe
Chathuri Ranasinghe <chathusudarshiranasinghe@gmail.com>
2025-01-12 11:27:14 +0530

    Modify home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -12,6 +12,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
@@ -42,23 +43,49 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-            CarouselSlider(
-              options: CarouselOptions(height: 400.0, autoPlay: true),
-              items: [1, 2, 3, 4, 5].map((i) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(color: Colors.amber),
-                        child: Text(
-                          'text $i',
-                          style: TextStyle(fontSize: 16.0),
-                        ));
-                  },
-                );
-              }).toList(),
-            )
+            FutureBuilder(
+                future: ApiService().getPopularMovies(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  List movies = snapshot.data!;
+                  return CarouselSlider(
+                    options: CarouselOptions(
+                        height: size.height * 0.22, autoPlay: true),
+                    items: movies.map((movie) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10.0, bottom: 10.0, left: 4.0, right: 4.0),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width,
+                              decoration: BoxDecoration(
+                                  color: Colors.grey.shade900,
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(movie.backdropPath))),
+                              child: Stack(
+                                children: [
+                                  Positioned(
+                                    bottom: 3,
+                                    left: 3,
+                                    child: Container(
+                                      padding: EdgeInsetsDirectional.symmetric(horizontal: 8),
+                                      decoration: BoxDecoration(color: Colors.black.withOpacity(0.6), borderRadius: BorderRadius.circular(20)),
+                                      child: Text(movie.title, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),)),
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+                  );
+                })
           ],
         ),
       ),
